0920ba2bb75d7f62e356c1b5ebb92a4adcdaecc7
My Name <my-name@chromium.org>
2019-01-15 22:41:03 +0800

    fix missing libxml

--- compile_example.bat
+++ compile_example.bat
@@ -1,14 +1,14 @@
 :: set environment
-set GYP_MSVS_VERSION=2015
+set GYP_MSVS_VERSION=2017
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 
 :: gn gen out
-call .\depot_tools\python .\depot_tools\gn.py gen .\out --ide=vs
+call D:\code\depot_tools D:\code\depot_tools\gn.py gen .\out --ide=vs
 
 :: gn args out
-call .\depot_tools\python .\depot_tools\gn.py args .\out
+call D:\code\depot_tools\python D:\code\depot_tools\gn.py args .\out
 
 :: compiling
-call .\depot_tools\ninja -C .\out example
+call D:\code\depot_tools\ninja -C .\out example
 
 pause
