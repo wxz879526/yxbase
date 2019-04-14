a82ce1ff9060410a716422869789a6088ce23e6b
My Name <my-name@chromium.org>
2019-04-14 23:31:07 +0800

    update

--- example/ConsoleDemo/ConsoleDemo/ConsoleDemo.cpp
+++ example/ConsoleDemo/ConsoleDemo/ConsoleDemo.cpp
@@ -42,6 +42,43 @@ int TaskDemo()
 	return  0;
 }
 
+class Foo {
+public:
+	virtual void Observe(int x) = 0;
+	virtual ~Foo() {};
+	virtual int GetValue() const { return 0; }
+};
+
+class Adder : public Foo {
+public:
+	explicit Adder(int scaler) : total(0), scaler_(scaler) {}
+	~Adder() override {}
+
+	virtual void Observe(int x) override
+	{
+		total += x * scaler_;
+	}
+
+	virtual int GetValue() const override
+	{
+		return total;
+	}
+
+	int total;
+
+private:
+	int scaler_;
+};
+
+class Disrupter : public Foo {
+
+private:
+	base::ObserverList<Foo>* list_;
+	Foo* doomed_;
+	bool remove_self_;
+};
+
+
 int main(int argc, char* argv[])
 {
 	base::CommandLine::Init(argc, argv);
@@ -73,14 +110,3 @@ int main(int argc, char* argv[])
 	system("Pause");
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
