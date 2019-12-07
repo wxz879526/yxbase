6aeeb4161c57c767fc87bf575b10d17b8bd31b13
My Name <my-name@chromium.org>
2019-12-07 14:15:13 +0800

    拷贝文件脚本

---
+++ copyfiles.ps1
@@ -0,0 +1,12 @@
+﻿
+echo "拷贝base"
+copy .\base\ .\pack\base\ -Recurse -Verbose -Force
+
+echo "拷贝gen"
+copy .\out\gen\ .\pack\out\gen\ -Recurse -Verbose -Force
+
+echo "拷贝lib"
+md .\pack\lib
+copy .\libcore.lib .\pack\lib\ -Verbose -Force
+
+Pause
