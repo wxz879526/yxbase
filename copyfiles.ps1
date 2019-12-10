f669bedeba06f8de807eb53e4a24eb0ffe05b4a8
My Name <my-name@chromium.org>
2019-12-10 21:12:34 +0800

    update

--- copyfiles.ps1
+++ copyfiles.ps1
@@ -2,8 +2,17 @@
 echo "拷贝base"
 copy .\base\ .\pack\base\ -Recurse -Verbose -Force
 
+echo "拷贝build"
+copy .\build\ .\pack\build\ -Recurse -Verbose -Force
+
 echo "拷贝gen"
 copy .\out\gen\ .\pack\out\gen\ -Recurse -Verbose -Force
+
+echo "拷贝testing"
+copy .\testing\ .\pack\testing\ -Recurse -Verbose -Force
+
+echo "拷贝third_party"
+copy .\third_party\googletest\ .\pack\third_party\googletest\ -Recurse -Verbose -Force
 
 echo "拷贝lib"
 md .\pack\lib
